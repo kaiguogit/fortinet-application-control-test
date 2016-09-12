d4b1e7daca44663a0516018f22e75ca9c6d55c27
kaiguogit <git@kaiguo.ca>
2016-09-12 10:54:03 -0700

    added show detail view

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,24 +1,12 @@
 $(() => {
- //http://api.jquery.com/jQuery.getJSON/
- //http://stackoverflow.com/questions/18637418/trying-to-load-local-json-file-to-show-data-in-a-html-page-using-jquery
-  // $.getJSON( "/test.json", function( signatures ) {
 
-  //   var $table = $("<table>");
-  //   var $tbody = $( "<tbody>", {
-  //     "class": "singatures"
-  //   })
 
-  //   signatures.splice(0, 3).forEach(function( signature, index) {
-  //     var $tr = $('<tr>').attr("id", index);
-  //     signature.forEach(function(columnData){
-  //      console.log("columnData", columnData);
-  //       var $td = $("<td>").html(columnData);
-  //        $td.appendTo($tr);
-  //     })
-  //     $tbody.append($tr);
-  //   });
-    
-  //   $tbody.appendTo( $table);
-  //   $table.appendTo("body");
-  // });
+  $('#signatures').DataTable({
+   "paging": true
+  });
+
+  $signatureLength_wrapper = $('#signatures_length').parent().get( 0 )
+  $signaturesFilter_wrapper = $('#signatures_filter').parent().get( 0 )
+  $($signaturesFilter_wrapper).after($($signatureLength_wrapper))
+ 
 });
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,34 @@ const express = require('express');
 const router  = express.Router();
 const data = require('../data.js');
 
+const dataField = ["Application Name","Type","Severe Level",
+ "Unknown Property","Unknown Property","Time", "Vendor","Vendor website",
+ "Network comsuption type","Description",
+ "Access Times", "Executable Name", "Available Actions",
+ "Unknown Property", "Bytes", "KB", "Brandwidth"]
+
 router.get("/", (req, res) => {
   res.render('index', {signatures: data});
 });
 
 router.get("/:id", (req, res) => {
-  res.render('show', {signature: data[req.params.id]});
+  res.render('show', {signature: mapFieldName(dataField, data[req.params.id])});
 });
 
+
+function mapFieldName(fields, values){
+ console.log("values passed in is", values);
+ var result;
+ if(values){
+   result = values.map(function(value, index){
+     return {field: fields[index], value: value};
+   });
+ }else{
+   result = fields.map(function(field){
+     return {field: field, value: null};
+   });
+ }
+ console.log(result);
+ return result;
+}
 module.exports = router;
