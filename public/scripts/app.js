a4a3dc89a52a8c348b4e911827d532e9e13662ae
kaiguogit <git@kaiguo.ca>
2016-09-12 12:18:31 -0700

    adjusted styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,11 @@
 $(() => {
 
-
+  //https://datatables.net/examples/styling/bootstrap.html
+  //Datatable plugin
   $('#signatures').DataTable({
-   "paging": true
+    "ajax": '/api/data'
   });
+
 
   $signatureLength_wrapper = $('#signatures_length').parent().get( 0 )
   $signaturesFilter_wrapper = $('#signatures_filter').parent().get( 0 )
--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,27 @@ const dataField = ["Application Name","Type","Severe Level",
  "Unknown Property", "Bytes", "KB", "Brandwidth"]
 
 router.get("/", (req, res) => {
-  res.render('index', {signatures: data});
+  res.render('index');
 });
+
+router.get("/api/data",  (req, res) => {
+  res.json({data: markATagOnName(data)});
+})
 
 router.get("/:id", (req, res) => {
   res.render('show', {signature: mapFieldName(dataField, data[req.params.id])});
 });
 
 
+function markATagOnName(array){
+  return array.slice(0).map(function(item, index){
+    var newItem = item.slice(0)
+    newItem[0] = `<a href=\"/${index}\">${newItem[0]}</a>`
+    return newItem;
+  })
+}
+
 function mapFieldName(fields, values){
- console.log("values passed in is", values);
  var result;
  if(values){
    result = values.map(function(value, index){
@@ -31,7 +42,8 @@ function mapFieldName(fields, values){
      return {field: field, value: null};
    });
  }
- console.log(result);
  return result;
 }
+
+
 module.exports = router;
